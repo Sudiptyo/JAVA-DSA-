5b97de94abdfe94f087375f661895a074b4c9649
Sudiptyo <sudiptyo444@gmail.com>
2026-04-27 03:00:35 +0530

    Add Quick Sort

--- SORT/QuickSort.java
+++ SORT/QuickSort.java
@@ -1,3 +1,60 @@
+
+class Search {
+
+    public void Display(int[] a) {
+        System.out.print("Sorted Array: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%d ", a[i]);
+        }
+    }
+
+    public int Partition(int[] a, int low, int high) {
+
+        int pivot = a[low];
+        int i = low + 1;
+        int j = high;
+        int t;
+
+        do {
+
+            while (i <= high && a[i] <= pivot) {
+                i++;
+            }
+
+            while (j >= low && a[j] > pivot) {
+                j--;
+            }
+
+            if (i < j) {
+                t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        } while (i < j);
+
+        t = a[low];
+        a[low] = a[j];
+        a[j] = t;
+
+        return j;
+    }
+
+    public void QuickSort(int[] a, int low, int high) {
+        if (low < high) {
+            int partitionIdx = Partition(a, low, high);
+            QuickSort(a, low, partitionIdx - 1);
+            QuickSort(a, partitionIdx + 1, high);
+        }
+    }
+}
+
 public class QuickSort {
-    
+    public static void main(String[] args) {
+
+        int[] a = { 5, 4, 3, 2, 1 };
+
+        Search s = new Search();
+        s.QuickSort(a, 0, a.length - 1);
+        s.Display(a);
+    }
 }
